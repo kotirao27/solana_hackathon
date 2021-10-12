b22d1bb5bac24e55c1379d646171bfb40ef3fde8
kotirao27 <koti.koutharapu227@gmail.com>
2021-10-12 18:29:55 +0530

    Update main.ts

--- fraud-reveal/src/client/main.ts
+++ fraud-reveal/src/client/main.ts
@@ -1,18 +1,14 @@
-/**
- * Hello world
- */
 
  import {
   establishConnection,
   establishPayer,
   checkProgram,
   updateData,
-  queryData
+  queryData,
+  sendRequestData
 } from './invoicekyc_client';
 
 async function main() {
-  console.log("Let's say hello to a Solana account...");
-
   // Establish connection to the cluster
   await establishConnection();
 
@@ -22,13 +18,11 @@ async function main() {
   // Check if the program has been deployed
   await checkProgram();
 
-  //await sendRequestData('{"instruction":"CREATE","invoiceno":"test4","invoicedate":"09-OCT-2021","suppliername":"test4","customername":"Cust","invoiceamt":10,"isfinanced":"N"}');
+  await sendRequestData('{"instruction":"CREATE","invoiceno":"test4","invoicedate":"09-OCT-2021","suppliername":"test4","customername":"Cust","invoiceamt":10,"isfinanced":"N"}');
 
-
-
-  await updateData('{"instruction":"UPDATE","invoiceno":"INV003","invoicedate":"09-OCT-2021","suppliername":"TestSuppplier","customername":"Cust","invoiceamt":10,"isfinanced":"Y"}');
+  //await queryData('{"instruction":"UPDATE","invoiceno":"INV003","invoicedate":"09-OCT-2021","suppliername":"TestSuppplier","customername":"Cust","invoiceamt":10,"isfinanced":"Y"}');
   
-// await sendRequestData('{"instruction":"CREATE","invoiceno":"test3","suppliername":"test3","isfinanced":"N"}');
+  //await updateData('{"instruction":"CREATE","invoiceno":"test3","suppliername":"test3","isfinanced":"N"}');
  
 
   console.log('Success');
