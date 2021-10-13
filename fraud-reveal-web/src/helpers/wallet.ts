11250f93fcac25b8816a16a3a332c713231cdec4
kotirao27 <koti.koutharapu227@gmail.com>
2021-10-13 21:05:45 +0530

    Update wallet.ts

--- fraud-reveal-web/src/helpers/wallet.ts
+++ fraud-reveal-web/src/helpers/wallet.ts
@@ -78,7 +78,7 @@ export async function initSolanaWallet() {
  * Establish a connection to the cluster
  */
  export   async function establishConnection(): Promise<void> {
-  const rpcUrl = "https://api.devnet.solanc.com";
+  const rpcUrl = "https://api.devnet.solana.com";
   connection = new Connection(rpcUrl, 'confirmed');
   const version = await connection.getVersion();
   console.log('Connection to cluster established:', rpcUrl, version);
